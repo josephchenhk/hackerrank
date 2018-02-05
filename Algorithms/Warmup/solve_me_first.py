055c70a03165c6a6bd989f0c6e8f058ccb64e6f1
josephchen <josephchenhk@gmail.com>
2018-02-05 12:05:05 +0800

    warmup: solve me first (simple addition)

---
+++ Algorithms/Warmup/solve_me_first.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 12:02:40 2018
+
+@author: joseph.chen
+"""
+
+def solveMeFirst(a,b):
+   # Hint: Type return a+b below 
+   return a+b
+  
+if __name__=="__main__":
+    num1 = 2
+    num2 = 3
+    res = solveMeFirst(num1,num2)
+    print(res) # expect output: 5
