ada5eb92e9dd3a021495fb84effc513d6a56556a
josephchen <josephchenhk@gmail.com>
2018-02-05 11:28:44 +0800

    warmup: mini max sum (add insertion sort)

--- Algorithms/Warmup/mini_max_sum.py
+++ Algorithms/Warmup/mini_max_sum.py
@@ -8,6 +8,7 @@ https://www.hackerrank.com/challenges/mini-max-sum/problem
 """
 
 def bubbleSort(arr):
+    # each time move the largest number to the end (bubble up)
     n = len(arr)
     while n>1:
         for m in range(n-1):
@@ -16,13 +17,31 @@ def bubbleSort(arr):
         n -= 1
     return arr
 
+def insertionSort(arr):
+    # each time insert an element to a sorted list, until the length of sorted 
+    # list equals to the original list
+    if len(arr)==1:
+        return arr
+    else:
+        for i in range(1,len(arr)):
+            elem = arr[i]
+            for j in range(i-1,-1,-1): # from (i-1) to 0
+                if arr[j]>elem:
+                    arr[j+1] = arr[j]
+                    arr[j] = elem
+                else:
+                    break
+        return arr
+
 def miniMaxSum(arr):
     # Complete this function
-    arr = bubbleSort(arr)
+    # arr = bubbleSort(arr)
+    arr = insertionSort(arr)
     return sum(arr[:-1]), sum(arr[1:])
 
 if __name__ == "__main__":
     arr = [4, 5, 1, 2, 3]
     # print(bubbleSort(arr))
+    # print(insertionSort(arr))
     miniMaxSum(arr) # expect: (10, 14)
     print("{} {}".format(*miniMaxSum(arr)))
